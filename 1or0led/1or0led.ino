e4461f1bf94e3a312753929bec2c3a27dd8bba72
keemhee <eirmonar@gmail.com>
2025-03-26 22:12:01 +0900

    led on/off

--- 1or0led/1or0led.ino
+++ 1or0led/1or0led.ino
@@ -1,26 +1,23 @@
-int led_pin = 13;
-char data;
+int led_pin = 13; // LED가 연결된 핀 번호를 13번으로 설정합니다.
+char data; // 시리얼 데이터를 저장할 변수를 선언합니다.
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(led_pin, OUTPUT);
+  Serial.begin(9600); // 시리얼 통신을 9600bps로 시작합니다.
+  pinMode(led_pin, OUTPUT); // LED 핀을 출력 모드로 설정합니다.
 }
 
 void loop() {
-  if (Serial.available() > 0) //총 몇개가 들어왔는지 알려줌.뭔가 시리얼을 통해 들어오면 이렇게 해라.
+  if (Serial.available() > 0) // 시리얼 통신으로부터 데이터가 들어온 개수를 확인합니다.
   {
-    data = Serial.read(); //하나씩 읽어줌.
-    Serial.println(data);
+    data = Serial.read(); // 시리얼 통신으로부터 하나의 데이터를 읽어옵니다.
+    Serial.println(data); // 읽어온 데이터를 시리얼 모니터에 출력합니다.
   }
-  if (data == '1')
+  if (data == '1') // 읽어온 데이터가 '1'이면
   {
-    digitalWrite(led_pin, HIGH);
-    
+    digitalWrite(led_pin, HIGH); // LED를 켭니다.
   }
-  else if (data == '0')
+  else if (data == '0') // 읽어온 데이터가 '0'이면
   {
-    digitalWrite(led_pin, LOW);
-    
+    digitalWrite(led_pin, LOW); // LED를 끕니다.
   }
-
 }
