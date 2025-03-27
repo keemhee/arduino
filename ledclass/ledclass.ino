9764a8d2ca0f587778ea79f87d6ee8d6d68702a9
keemhee <eirmonar@gmail.com>
2025-03-27 17:13:59 +0900

    Update ledclass.ino
    LED 클래스, 헤더파일 적용 연습

--- ledclass/ledclass.ino
+++ ledclass/ledclass.ino
@@ -1,15 +1,14 @@
-#include "LED.h"
+#include "LED.h" // LED 클래스의 헤더 파일을 포함합니다.
 
-LED myLed(13);
+LED myLed(13); // 핀 13에 연결된 LED 객체를 생성합니다.
 
 void setup() {
-  myLed.begin();
-
+  myLed.begin(); // LED를 초기화합니다.
 }
 
 void loop() {
-  myLed.on();
-  delay(1000);
-  myLed.off();
-
+  myLed.on(); // LED를 켭니다.
+  delay(1000); // 1초 동안 대기합니다.
+  myLed.off(); // LED를 끕니다.
+  delay(1000); // 1초 동안 대기합니다.
 }
