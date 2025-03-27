2974d19a8bc43851684cc5bf6c62166a1c532db6
keemhee <eirmonar@gmail.com>
2025-03-27 17:15:17 +0900

    Update ledbutton.ino
    버튼 눌리면 led on, 아니면 led off

--- ledbutton/ledbutton.ino
+++ ledbutton/ledbutton.ino
@@ -1,24 +1,19 @@
-int button_pin = 7;
-int input = 0;
+int button_pin = 7; // 버튼이 연결된 핀 번호를 저장합니다.
+int input = 0; // 버튼의 입력 상태를 저장할 변수를 선언합니다.
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(button_pin, INPUT);
-  pinMode(5, OUTPUT);
+void setup() {
+  Serial.begin(9600); // 시리얼 통신을 시작합니다.
+  pinMode(button_pin, INPUT); // 버튼 핀을 입력 모드로 설정합니다.
+  pinMode(5, OUTPUT); // 핀 5를 출력 모드로 설정합니다.
 }
 
-void loop()
-{
-  input = digitalRead(button_pin);
-  Serial.println(input);
-  if(input == 1)
-  {
-    digitalWrite(5, HIGH);
+void loop() {
+  input = digitalRead(button_pin); // 버튼 핀의 입력 상태를 읽어서 input 변수에 저장합니다.
+  Serial.println(input); // 버튼의 입력 상태를 시리얼 모니터에 출력합니다.
+  if (input == 1) { // 버튼이 눌렸을 때 (입력이 HIGH일 때)
+    digitalWrite(5, HIGH); // 핀 5에 연결된 LED를 켭니다.
+  } else { // 버튼이 눌리지 않았을 때 (입력이 LOW일 때)
+    digitalWrite(5, LOW); // 핀 5에 연결된 LED를 끕니다.
   }
-  else
-  {
-    digitalWrite(5, LOW);
-  }
-  delay(100);
+  delay(100); // 100밀리초 동안 대기합니다.
 }
