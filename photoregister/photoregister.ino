1641087acdf081b9937bc9a276122b8a0fa19895
keemhee <eirmonar@gmail.com>
2025-03-27 16:21:32 +0900

    Update photoregister.ino
    조도 센서(photoregister)의 입력 값을 읽고, 그 값에 따라 LED on/off
    조도 센서의 출력 값이 특정 임계값보다 낮으면 LED 켜고, 그렇지 않으면 LED 끔
    조도 센서의 값 시리얼 모니터에 출력

--- photoregister/photoregister.ino
+++ photoregister/photoregister.ino
@@ -1,22 +1,23 @@
 void setup()
 {
-  pinMode(A0, INPUT); // input은 생략해도 읽힘
-  pinMode(8, OUTPUT);
-  Serial.begin(9600); // 시리얼 모니터를 사용하겠다. 그래야 볼 수 있음. 
-  					  // 파라미터로 속도 받음.9600은 일반적인 속도
+  pinMode(A0, INPUT); // A0 핀을 입력 모드로 설정 (포토레지스터 연결)
+  pinMode(8, OUTPUT); // 8번 핀을 출력 모드로 설정 (LED 연결)
+  Serial.begin(9600); // 시리얼 통신을 9600 baud로 시작, 시리얼 모니터 사용
 }
 
 void loop()
 {
-  int brightness = analogRead(A0);
-  Serial.println(brightness); // 프린트할 때 라인바꾸기.
-  if (brightness <10 )
+  int brightness = analogRead(A0); // A0 핀에서 아날로그 값을 읽어 밝기 변수에 저장
+  Serial.println(brightness); // 읽은 밝기 값을 시리얼 모니터에 출력 (줄 바꿈)
+  
+  if (brightness < 10) // 만약 밝기 값이 10보다 작으면 (어두운 경우)
   {
-    digitalWrite(8, HIGH);
+    digitalWrite(8, HIGH); // 8번 핀을 HIGH로 설정하여 LED를 켬
   }
-  else 
+  else // 그렇지 않으면 (밝은 경우)
   {
-    digitalWrite(8, LOW);
+    digitalWrite(8, LOW); // 8번 핀을 LOW로 설정하여 LED를 끔
   }
-  delay(100);
+  
+  delay(100); // 100ms 대기
 }
