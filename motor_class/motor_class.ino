a03d5adf9aac11e668615d51752aa9f4b6ab348d
keemhee <eirmonar@gmail.com>
2025-03-27 17:11:51 +0900

    Update motor_class.ino
    motor 클래스, 헤더파일 만들어 적용해보기

--- motor_class/motor_class.ino
+++ motor_class/motor_class.ino
@@ -1,15 +1,15 @@
-#include "MOTOR.h"
+#include "MOTOR.h" // MOTOR 클래스의 헤더 파일을 포함합니다.
 
-MOTOR MOTOR(5, 6);
+MOTOR motor(5, 6); // 핀 5와 6에 연결된 모터 객체를 생성합니다.
 
 void setup() {
-  motor.begin();
+  motor.begin(); // 모터를 초기화합니다.
 }
 
 void loop() {
-  motor.forward();
-  delay(100);
-  motor.stop();
-  delay(100);
-  motor.backward();
+  motor.forward(); // 모터를 앞으로 이동시킵니다.
+  delay(100); // 100밀리초 동안 대기합니다.
+  motor.stop(); // 모터를 정지시킵니다.
+  delay(100); // 100밀리초 동안 대기합니다.
+  motor.backward(); // 모터를 뒤로 이동시킵니다.
 }
